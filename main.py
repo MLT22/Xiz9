45128e9ee8cb2f0306175c0aa72b903316c65832
João Carlos de Oliveira Serrano <JoaoCarlosSerrano@outlook.com>
2026-05-14 20:27:29 -0300

    Chamada das analises

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controller.bronze_controller import bronze_router
- 
+
 app = FastAPI(title="Image Metadata API")
- 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(bronze_router)
